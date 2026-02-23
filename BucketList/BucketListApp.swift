de567e8e73b93837187dee0df5ad9221941ea552
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-23 13:37:21 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Alonso Acosta on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
