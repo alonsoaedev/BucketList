1b7c43f3ac94ef558b49f3548131b65f4ad69d6e
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-24 13:02:35 -0600

    Switch positions in MapKit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,43 +5,58 @@
 //  Created by Alonso Acosta on 23/02/26.
 //
 
+import MapKit
 import SwiftUI
 
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed!")
-    }
-}
-
 struct ContentView: View {
-    @State private var loadingState: LoadingState = .loading
-    
-    enum LoadingState {
-        case loading
-        case success
-        case failed
-    }
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid(elevation: .realistic))
+                .onMapCameraChange { context in
+                    print("Finished at")
+                    print(context.region)
+                }
+                .onMapCameraChange(frequency: .continuous) { context in
+                    print("Continuous update")
+                    print(context.region)
+                }
+            
+            HStack(spacing: 50) {
+                Button("London") {
+                    position = .region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+                
+                Button("Paris") {
+                    position = .region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.856613, longitude: 2.352222),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+                
+                Button("Tokyo") {
+                    position = .region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6895, longitude: 139.6917),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
         }
     }
 }
