c43f12a0f925f0a9cc430a5f83be171567758e65
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-24 13:08:36 -0600

    Add markers on the map

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,53 +8,32 @@
 import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        )
-    )
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     
     var body: some View {
         VStack {
-            Map(position: $position)
-                .mapStyle(.hybrid(elevation: .realistic))
-                .onMapCameraChange { context in
-                    print("Finished at")
-                    print(context.region)
-                }
-                .onMapCameraChange(frequency: .continuous) { context in
-                    print("Continuous update")
-                    print(context.region)
-                }
-            
-            HStack(spacing: 50) {
-                Button("London") {
-                    position = .region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-                
-                Button("Paris") {
-                    position = .region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 48.856613, longitude: 2.352222),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-                
-                Button("Tokyo") {
-                    position = .region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 35.6895, longitude: 139.6917),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
+            Map {
+                ForEach(locations) { location in
+//                    Marker(location.name, coordinate: location.coordinate)
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Text(location.name)
+                            .font(.headline)
+                            .padding()
+                            .background(.blue.gradient)
+                            .foregroundStyle(.white)
+                            .clipShape(.capsule)
+                    }
+                    .annotationTitles(.hidden)
                 }
             }
         }
