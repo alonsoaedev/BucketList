6ddb67447f6cecd90d6dab89d259eb5b9f0bdf69
Alonso Acosta <alonso.ae@hotmail.com>
2026-02-25 23:02:35 -0600

    Add user locations to a map

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,39 +5,45 @@
 //  Created by Alonso Acosta on 23/02/26.
 //
 
-import LocalAuthentication
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isUnlocked: Bool = false
+    @State private var locations: [Location] = []
+    
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
     
     var body: some View {
-        VStack {
-            if isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
-        }
-        .onAppear(perform: authenticate)
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data."
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
-                if success {
-                    isUnlocked = true
-                } else {
-                    // there was a problem
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Marker(
+                        location.name,
+                        coordinate: CLLocationCoordinate2D(
+                            latitude: location.latitude,
+                            longitude: location.longitude
+                        )
+                    )
                 }
             }
-        } else {
-            // no biometrics
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local ) {
+                    locations.append(
+                        Location(
+                            id: UUID(),
+                            name: "New Location",
+                            description: "",
+                            latitude: coordinate.latitude,
+                            longitude: coordinate.longitude
+                        )
+                    )
+                }
+            }
         }
     }
 }
